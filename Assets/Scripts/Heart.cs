2b546358b61923b622147177f0e4b360355e5908
SergheiDerevenco <47867265+SergheiDerevenco@users.noreply.github.com>
2020-05-13 19:14:45 +0300

    Add files via upload

---
+++ Assets/Scripts/Heart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+    	Character character = collider.GetComponent<Character>();
+    	if (character)
+    	{
+    		character.Lives++;
+    		Destroy(gameObject);
+    	}
+    }
+}
